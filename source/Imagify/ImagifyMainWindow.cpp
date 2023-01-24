62ac31ada29c92254b5fb7e2392e862c2b99f065
konstantysz <kostekszumigaj@gmail.com>
2023-01-24 17:08:56 +0100

    First version of image loading

--- source/Imagify/ImagifyMainWindow.cpp
+++ source/Imagify/ImagifyMainWindow.cpp
@@ -1,6 +1,5 @@
 #include "ImagifyMainWindow.h"
 #include "ui_ImagifyMainWindow.h"
-#include "FileIO.h"
 
 ImagifyMainWindow::ImagifyMainWindow(QWidget* parent) :
     QMainWindow(parent),
@@ -8,7 +7,7 @@ ImagifyMainWindow::ImagifyMainWindow(QWidget* parent) :
 {
     ui->setupUi(this);
 
-    static const char* status_waiting = "Waiting for input...";
+    const auto status_waiting = "Waiting for input...";
     status = new QLabel(status_waiting);
     ui->statusBar->addWidget(status);
 
@@ -25,7 +24,14 @@ void ImagifyMainWindow::OnLoadButtonClicked()
 {
     try
     {
-        const auto image = Core::LoadImage(ui->fileLineEdit->text().toStdString());
+        const auto image = CvMatToQImage(Core::LoadImage(ui->fileLineEdit->text().toStdString()));
+        QPixmap imagePixelMap = QPixmap::fromImage(image);
+        ui->inputImage->setPixmap(
+            imagePixelMap.scaled(ui->inputImage->width(), ui->inputImage->height(), Qt::KeepAspectRatio));
+        ui->inputImage->adjustSize();
+        ui->inputImage->setScaledContents(true);
+        ui->inputImage->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+        ui->inputImage->show();
     }
     catch (const std::invalid_argument& exception)
     {
@@ -39,4 +45,44 @@ void ImagifyMainWindow::DisplayStatus(const QString& statusMessage)
     ui->statusBar->removeWidget(status);
     status = new QLabel(statusMessage);
     ui->statusBar->addWidget(status);
+}
+
+QImage ImagifyMainWindow::CvMatToQImage(const cv::Mat& cvMatImage)
+{
+    // according to: https://asmaloney.com/2013/11/code/converting-between-cvmat-and-qimage-or-qpixmap/
+    switch (cvMatImage.type())
+    {
+    // 8-bit, 4 channel
+    case CV_8UC4:
+    {
+        QImage image(cvMatImage.data,
+            cvMatImage.cols, cvMatImage.rows,
+            static_cast<int>(cvMatImage.step),
+            QImage::Format_ARGB32);
+        return image.copy();
+    }
+    // 8-bit, 3 channel
+    case CV_8UC3:
+    {
+        QImage image(cvMatImage.data,
+            cvMatImage.cols, cvMatImage.rows,
+            static_cast<int>(cvMatImage.step),
+            QImage::Format_RGB888);
+        return image.rgbSwapped();
+    }
+    // 8-bit, 1 channel
+    case CV_8UC1:
+    {
+        QImage image(cvMatImage.data,
+            cvMatImage.cols, cvMatImage.rows,
+            static_cast<int>(cvMatImage.step),
+            QImage::Format_Grayscale8);
+        return image.copy();
+    }
+    default:
+        std::invalid_argument(std::to_string(cvMatImage.type()) + " cv::Mat image type not handled in CvMatToQImage");
+        break;
+    }
+
+    return QImage();
 }
--- source/Imagify/ImagifyMainWindow.h
+++ source/Imagify/ImagifyMainWindow.h
@@ -1,30 +1,50 @@
 #pragma once
+#include "FileIO.h"
+
 #include <QLabel>
 #include <QMainWindow>
 #include <QLineEdit>
 #include <QPushButton>
 
+/*! \file ImagifyMainWindow.h
+    Contains main window of the Imagify app.
+*/
 namespace Ui {
     class ImagifyMainWindow;
 }
 
+/*! \class ImagifyMainWindow
+	Class that handles logic of the Imagify main window.
+*/
 class ImagifyMainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /*! Class constructor.
+        \param[in] parent Widget that is parent to this window. Default root.
+    */
     explicit ImagifyMainWindow(QWidget* parent = 0);
 
+    //! Class destructor.
     ~ImagifyMainWindow();
 
 private slots:
+    //! Method that handles event when load button is clicked.
     void OnLoadButtonClicked();
 
 private:
+    /*! Method that displays status message in window.
+        \param[in] statusMessage Message to be displayed.
+    */
     void DisplayStatus(const QString& statusMessage);
 
-    Ui::ImagifyMainWindow* ui;
-    QLabel* status;
-    QLineEdit* file;
-    QPushButton* loadButton;
+    /*! Method for converting cv::Mat to QImage.
+        \param[in] cvMatImage cv mat image.
+        \returns Converted image.
+    */
+    QImage CvMatToQImage(const cv::Mat& cvMatImage);
+
+    Ui::ImagifyMainWindow* ui;  //!< Main window UI.
+    QLabel* status;             //!< Status of the program.
 };
